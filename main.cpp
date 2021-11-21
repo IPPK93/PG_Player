ad2c4953f56b77f998a3523aa65ae3011ae3e2cd
glebny <58235662+glebny@users.noreply.github.com>
2021-11-21 16:53:07 +0300

    delete // and cout

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-//    return a.exec();
-    std::cout << "11" + 1 << std::endl;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
