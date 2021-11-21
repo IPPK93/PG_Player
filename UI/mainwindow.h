6f110594eb7c8e2456ee213447c2a24332dc7ccc
glebny <58235662+glebny@users.noreply.github.com>
2021-11-21 16:50:47 +0300

    add Aimp style player model

--- UI/mainwindow.h
+++ UI/mainwindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QStandardItemModel>
+#include <QtMultimedia/QMediaPlayer>
+#include <QtMultimedia/QMediaPlaylist>
+#include <QMouseEvent>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -10,12 +13,45 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    Q_PROPERTY(QPoint previousPosition READ previousPosition WRITE setPreviousPosition NOTIFY previousPositionChanged)
+    
+    enum MouseType {
+        None = 0,
+        Top,
+        Bottom,
+        Left,
+        Right,
+        Move
+    };
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QPoint previousPosition() const;
+    
+public slots:
+    void setPreviousPosition(QPoint previousPosition);  
+    
+signals:
+    void previousPositionChanged(QPoint previousPosition);
+
+protected:
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+private slots:
+    void on_btn_add_clicked(); // Слот для обработки добавления треков через диалоговое окно
+//    void abc();
 
 private:
     Ui::MainWindow *ui;
+    QStandardItemModel  *m_playListModel;   // Модель данных плейлиста для отображения
+    QMediaPlayer        *m_player;          // Проигрыватель треков
+    QMediaPlaylist      *m_playlist;        // Плейлиста проигрывателя
+    MouseType m_leftMouseButtonPressed;
+    QPoint m_previousPosition;
+
+    MouseType checkResizableField(QMouseEvent *event);
 };
 #endif // MAINWINDOW_H
