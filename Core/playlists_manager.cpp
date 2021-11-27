3da173a8e162ee841abf2b8fcad40d589c15ad04
IPPK93 <61944163+IPPK93@users.noreply.github.com>
2021-11-27 20:19:48 +0300

    Add models & data + implement the basic part of the constructor

--- Core/playlists_manager.cpp
+++ Core/playlists_manager.cpp
@@ -1,5 +1,23 @@
 #include "playlists_manager.h"
 
+PlaylistsManager::PlaylistsManager(QObject* parent)
+    : QObject(parent)
+{
+    this->playlists = new QMediaPlaylist;
+    this->playlists_model = new QStandardItemModel(this);
+
+    this->default_playlist = new QMediaPlaylist;
+
+    this->playlists->addMedia(default_playlist);
+    this->playlists_model->appendRow(new QStandardItem("Default"));
+    this->playlists_model->setHorizontalHeaderLabels(QStringList()  << tr("Playlist"));
+
+
+    this->cur_playlist = default_playlist;
+    this->cur_playlist_model = new QStandardItemModel(this);
+    this->cur_playlist_model->setHorizontalHeaderLabels(QStringList()  << tr("Audio Track"));
+}
+
 /*!
  * \brief PlaylistsManager::create_playlist
  * \param name
@@ -9,9 +27,10 @@
  *
  * NOTE: Parameters are still under consideration
  */
-void PlaylistsManager::create_playlist(const std::string &name, std::vector<Music *> &data)
+void PlaylistsManager::create_playlist(QMediaPlaylist* playlist, QList<QList<QStandardItemModel*>>* playlist_info)
 {
-
+//    this->playlists->addMedia(playlist);
+//    this->playLists_model->appendRow(playlist_info);
 }
 
 /*!
@@ -19,8 +38,13 @@ void PlaylistsManager::create_playlist(const std::string &name, std::vector<Musi
  * \param name
  *
  * \details Delete playlist with a given name
+ *
+ * NOTE: Parameters are still under consideration
  */
-void PlaylistsManager::delete_playlist(const std::string &name)
+void PlaylistsManager::delete_playlist(const QString& name)
 {
 
 }
+
+PlaylistsManager::~PlaylistsManager()
+{ }
--- Core/playlists_manager.h
+++ Core/playlists_manager.h
@@ -1,22 +1,36 @@
 #ifndef PLAYLISTS_MANAGER_H
 #define PLAYLISTS_MANAGER_H
 
-#include <vector>
-#include "playlist.h"
+#include <QVector>
+#include <QStandardItemModel>
+#include <QMediaPlaylist>
+#include <QString>
+#include <QObject>
+
 
 /*!
  * \brief The PlaylistsManager class
  *
  * \details Class that regulates playlists workflow
  */
-class PlaylistsManager
+class PlaylistsManager : public QObject
 {
+    Q_OBJECT
 public:
-    std::vector<Playlist*> playlists;
-    Playlist** current_playlist;
+    explicit PlaylistsManager(QObject* parent = nullptr);
 
-    void create_playlist(const std::string& name, std::vector<Music*>& data); //???
-    void delete_playlist(const std::string& name);
+    QMediaPlaylist* playlists;
+    QMediaPlaylist* cur_playlist;
+    QMediaPlaylist* default_playlist;
+
+    QStandardItemModel* playlists_model; // Модель данных плейлистов для отображения
+    QStandardItemModel* cur_playlist_model; // Модель данных плейлистов для отображения
+
+
+    void create_playlist(QMediaPlaylist* playlist, QList<QList<QStandardItemModel*>>* playlist_info); //???
+    void delete_playlist(const QString& name);
+
+    virtual ~PlaylistsManager() override;
 };
 
 #endif // PLAYLISTS_MANAGER_H
