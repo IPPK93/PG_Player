a0f98bcbafab2032e5eb897c5794bd324a716482
glebny <58235662+glebny@users.noreply.github.com>
2021-12-10 13:39:21 +0300

    add connects and more functions

--- player.cpp
+++ player.cpp
@@ -20,19 +20,97 @@ Player::Player(QObject* parent)
     {
         handle_folder();
     });
+        
+    connect(ui->get_play_button(), &QToolButton::clicked, [this]()
+    {
+        play_button();
+    });
 
+    connect(ui->get_shuffle_button(), &QToolButton::clicked, [this]()
+    {
+        shuffle_button();
+    });
+
+    connect(ui->get_slider_vol(), &QSlider::valueChanged, [this](int value)
+    {
+        core->track_controller->setVolume(value);
+    });
+
+    connect(ui->get_slider_track(), &QSlider::sliderMoved, [this](int value)
+    {
+        on_sldr_track_valueChanged(value);
+        //core->track_controller->setVolume(value);
+    });        
+    
     connect(ui->get_prev_button(), &QToolButton::clicked, core->cur_playlist(), &QMediaPlaylist::previous);
     connect(ui->get_next_button(), &QToolButton::clicked, core->cur_playlist(), &QMediaPlaylist::next);
-    connect(ui->get_play_button(), &QToolButton::clicked, core->track_controller, &QMediaPlayer::play);
-    connect(ui->get_pause_button(), &QToolButton::clicked, core->track_controller, &QMediaPlayer::pause);
+    //connect(ui->get_play_button(), &QToolButton::clicked, core->track_controller, &QMediaPlayer::play);
+   // connect(ui->get_pause_button(), &QToolButton::clicked, core->track_controller, &QMediaPlayer::pause);
     connect(ui->get_stop_button(), &QToolButton::clicked, core->track_controller, &QMediaPlayer::stop);
-
+        
     connect(ui->get_playlist_view(), &QTableView::doubleClicked, [this](const QModelIndex &index){
         core->cur_playlist()->setCurrentIndex(index.row());
     });
 
 
     ui->show();
+}
+
+void Player::play_button()
+{
+    if(play_clicked == false)
+    {
+        play_clicked = true;
+        core->track_controller->play();
+        ui->set_pause_button();
+    }
+    else
+    {
+        play_clicked = false;
+        core->track_controller->pause();
+        ui->set_play_button();
+    }
+}
+
+void Player::shuffle_button()
+{
+    switch (shuffle_clicked)
+        {
+        case 0:
+            shuffle_clicked++;
+            ui->set_shuffle_button();
+            core->playlists_manager->cur_playlist->setPlaybackMode(QMediaPlaylist::Random);
+            break;
+        case 1:
+            shuffle_clicked++;
+            ui->set_repeat_button();
+            core->playlists_manager->cur_playlist->setPlaybackMode(QMediaPlaylist::Loop);
+           // m_playlist->setPlaybackMode(QMediaPlaylist::Loop);
+            break;
+        case 2:
+            shuffle_clicked++;
+            ui->set_repeat_one_button();
+            core->playlists_manager->cur_playlist->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+           // m_playlist->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+            break;
+        case 3:
+            shuffle_clicked = 0;
+            ui->set_direct_play_button();
+            core->playlists_manager->cur_playlist->setPlaybackMode(QMediaPlaylist::Sequential);
+          //  m_playlist->setPlaybackMode(QMediaPlaylist::Sequential);
+            break;
+        }
+}
+
+//void Player::on_sldr_vol_valueChanged(int value)
+//{
+//    //m_player->setVolume(value)
+//}
+
+void Player::on_sldr_track_valueChanged(int value)
+{
+    core->track_controller->setNotifyInterval(10);
+    core->track_controller->setPosition(value);
 }
 
 void Player::handle_folder()
